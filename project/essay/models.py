9c93d18ba0f95c6731256233b34141472ddb3565
Hamidrezashafaei <hamidreshtime@gmail.com>
2024-01-10 22:29:11 +0330

    update 3

--- project/essay/models.py
+++ project/essay/models.py
@@ -2,6 +2,9 @@
 
 from django.db import models
 from authors.models import Profile_authors
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 
 class Essay(models.Model):
     author = models.ForeignKey(Profile_authors , null=True
@@ -12,7 +15,13 @@ class Essay(models.Model):
 
 
     def __str__(self):
-        return self.title 
+        return self.title if self.title else f"Untitled Essay {self.pk}"
+
+@receiver(post_save, sender=Essay)
+def update_title_variable(sender, instance, **kwargs):
+    if instance.title is None and instance.pk:
+        instance.title = f"Article {instance.pk}"
+        instance.save()
 
 class Comment(models.Model):
     essay = models.ForeignKey(Essay, on_delete=models.CASCADE)
